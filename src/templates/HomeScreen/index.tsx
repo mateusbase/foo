9b8263d54854ff8d8eb00cc2ff816cea12b2f335
Victor Saraiva <vf.ramos7@gmail.com>
2024-08-19 16:39:03 -0300

    adding the menu with main options in header

--- src/templates/HomeScreen/index.tsx
+++ src/templates/HomeScreen/index.tsx
@@ -1,10 +1,20 @@
-import { Search } from 'lucide-react';
+import { IoIosCalendar } from "react-icons/io";
+import { FaUser, FaMapMarkerAlt, FaFile  } from "react-icons/fa";
+
 import SearchInput from './components/SearchInput';
+import MainOptionsActions from '@/components/MainOptionsActions';
 
 export default function HomeScreen(): JSX.Element {
+  const options = [
+    { title: 'Agende uma consulta ou exame', icon: IoIosCalendar  },
+    { title: 'Encontre uma unidade', icon: FaMapMarkerAlt },
+    { title: 'Encontre um médico', icon: FaUser },
+    { title: 'Resultado de Exames', icon: FaFile }
+  ];
+
   return (
-    <main className="flex flex-wrap gap-4">
-      <div className="relative w-full">
+    <main className="relative flex flex-wrap gap-4">
+      <div className="w-full">
         <img
           src="https://placehold.co/1920x600/000000/FFF"
           alt=""
@@ -12,6 +22,10 @@ export default function HomeScreen(): JSX.Element {
         />
         <div className="absolute inset-0 flex items-center justify-center">
           <SearchInput />
+        </div>
+
+        <div className="absolute inset-0 flex items-end justify-center">
+          <MainOptionsActions options={options} />
         </div>
       </div>
     </main>
