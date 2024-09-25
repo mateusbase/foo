a1d49ac1f14cf82f61551e973c5cc4d54e677052
Felipe <flipsantiago@hotmail.com>
2024-09-25 14:33:58 -0300

    fix: :bug:

--- src/templates/FindAUnitScreen/index.tsx
+++ src/templates/FindAUnitScreen/index.tsx
@@ -12,26 +12,32 @@ import PageHeader from "@/components/PageHeader";
 export default function FindAUnitScreen(): JSX.Element {
   return (
     <main>
+      <div className="block bg-gray-200 p-6 md:hidden">
+        <Breadcrumb />
+      </div>
+
       <PageHeader
         title="Encontre uma unidade"
         subtitle="Conheça as unidades Oncoclínicas em todo o Brasil"
         showContactSection={false}
       />
 
-      <BaseContainer className="flex w-full flex-col py-20">
-        <Breadcrumb />
+      <BaseContainer className="flex w-full flex-col py-0 md:py-20">
+        <div className="hidden px-10 md:block md:px-0">
+          <Breadcrumb />
+        </div>
 
         <div className="mt-14">
-          <h1 className="font-lato text-4xl leading-[48px] text-primary">
+          <h1 className="font-lato hidden text-4xl leading-[48px] text-primary md:block">
             Encontre uma unidade Oncoclínicas próxima a você
           </h1>
 
-          <div className="mt-14 flex items-center justify-between">
+          <div className="mt-0 flex flex-col gap-5 md:mt-14 md:flex-col lg:flex-row lg:items-center lg:justify-center">
             <Select
               color="primary"
               variant="bordered"
               label="UF"
-              className="max-w-xs"
+              className="w-full md:max-w-full lg:max-w-[122px]"
               radius="full"
               size="sm"
             >
@@ -47,7 +53,7 @@ export default function FindAUnitScreen(): JSX.Element {
               color="primary"
               variant="bordered"
               label="Cidade"
-              className="max-w-xs"
+              className="w-full md:max-w-full lg:max-w-xs"
               radius="full"
               size="sm"
             >
@@ -64,33 +70,39 @@ export default function FindAUnitScreen(): JSX.Element {
               size="lg"
               radius="full"
               variant="bordered"
-              className="h-[50px] w-[280px]"
+              className="h-[50px] w-full"
             />
 
-            <BaseButton color="primary" className="text-white">
-              Buscar Unidades
-            </BaseButton>
+            <div className="flex w-full flex-col items-center gap-4 md:flex-row lg:flex-row lg:gap-4">
+              <BaseButton
+                color="primary"
+                className="w-full text-white lg:w-auto"
+                width="100% lg:w-[322px]"
+              >
+                Buscar Unidades
+              </BaseButton>
 
-            <BaseButton
-              className="font-bold"
-              color="primary"
-              variant="bordered"
-              startContent={<TbCodePlus size={20} />}
-              width="322px"
-            >
-              Buscar próximos a mim
-            </BaseButton>
+              <BaseButton
+                className="w-full font-bold lg:w-auto"
+                color="primary"
+                variant="bordered"
+                startContent={<TbCodePlus size={20} />}
+                width="100% lg:w-[322px]"
+              >
+                Buscar próximos a mim
+              </BaseButton>
+            </div>
           </div>
 
-          <p className="font-lato mt-20 text-[20px] font-normal leading-[26px] text-darkGray">
+          <p className="font-lato mt-14 text-[20px] font-normal leading-[26px] text-darkGray md:mt-20">
             Sua pesquisa encontrou 223 unidades
           </p>
 
-          <section className="mb-10 mt-20 flex justify-between">
+          <div className="mx-auto mb-10 mt-20 grid w-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
             {unitsData.map((unit) => (
               <UnitsCard key={unit.id} {...unit} />
             ))}
-          </section>
+          </div>
         </div>
       </BaseContainer>
 
