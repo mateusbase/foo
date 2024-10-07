34b0ea2fd4e6acef4a0ca69aa5c184c20922b0ae
Felipe <flipsantiago@hotmail.com>
2024-10-07 16:29:05 -0300

    fix: :bug:

--- src/templates/ServicesScreen/components/ServiceSection/index.tsx
+++ src/templates/ServicesScreen/components/ServiceSection/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from "react";
-import { Button, Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { TbCodePlus } from "react-icons/tb";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
@@ -11,6 +11,7 @@ import { useRef } from "react";
 import { NavigationOptions } from "swiper/types";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import AlphabetSelector from "@/components/AlphabetSelector";
+import BaseInput from "@/components/Input";
 import SectionHeader from "../SectionHeader";
 import { ServiceSectionProps } from "../../types";
 
@@ -37,12 +38,11 @@ export default function ServiceSection({
       <SectionHeader title={title} description={description} />
 
       <div className="mt-10 flex w-full lg:hidden">
-        <Input
+        <BaseInput
           placeholder="Buscar serviço"
           size="lg"
           radius="full"
           variant="bordered"
-          color="primary"
           endContent={<Search className="text-2xl text-primary" />}
         />
       </div>
