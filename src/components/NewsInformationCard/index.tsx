032d0673c9e7f3986d0c874e8cb34770188fef34
Luiz Fernando <grexd@hotmail.com>
2024-11-07 11:55:03 -0300

    feat: :sparkles: creating blog screen mobile and tablet layout

--- src/components/NewsInformationCard/index.tsx
+++ src/components/NewsInformationCard/index.tsx
@@ -7,32 +7,32 @@ export default function NewsInformationCard({
   const { title, date, description, image } = options;
 
   return (
-    <div className="mx-auto flex w-full gap-8">
-      <div className="h-auto w-[812px] shrink-0">
+    <div className="mx-auto flex w-full gap-4 md:flex-col lg:flex-row lg:gap-8">
+      <div className="h-auto shrink-0 md:max-w-[322px] lg:max-w-[812px]">
         <img
           src={image}
           alt="ASCO Annual Meeting 2024"
-          className="size-full rounded-md object-cover"
+          className="size-full object-cover"
         />
       </div>
 
       <div className="flex w-full flex-col justify-start">
         <div>
-          <BaseButton variant="bordered" color="secondary">
+          <BaseButton className="text-sm" variant="bordered" color="secondary">
             <LuArrowUpRight />
             Inovação & Oncologia
           </BaseButton>
         </div>
 
-        <h1 className="mt-8 text-left font-lato-light text-[48px] leading-[48px] text-darkGray">
+        <h1 className="mt-8 text-left font-lato-light text-xl text-darkGray lg:text-[48px] lg:leading-[48px]">
           {title}
         </h1>
 
-        <p className="mt-4 text-left text-[16px] font-normal leading-[19px] text-primary">
+        <p className="mt-4 text-left text-sm font-normal text-primary lg:text-[16px] lg:leading-[19px]">
           {date}
         </p>
 
-        <p className="mt-14 text-left text-xl font-medium leading-[27px] text-darkGray">
+        <p className="mt-14 text-left text-sm font-medium text-darkGray lg:text-xl lg:leading-[27px]">
           {description}
         </p>
       </div>
--- src/templates/BlogScreen/index.tsx
+++ src/templates/BlogScreen/index.tsx
@@ -6,12 +6,24 @@ import { FaRunning } from "react-icons/fa";
 import { MdArrowOutward } from "react-icons/md";
 import { GiFlowerStar } from "react-icons/gi";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { IoIosArrowDown } from "react-icons/io";
+import {
+  IoIosArrowBack,
+  IoIosArrowDown,
+  IoIosArrowForward,
+} from "react-icons/io";
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
 import { LuArrowUpRight } from "react-icons/lu";
+import BaseSelect from "@/components/Select";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { NavigationOptions } from "swiper/types";
+import { Navigation } from "swiper/modules";
+import { useRef } from "react";
 import { options } from "./optionsMock";
 
 export default function BlogScreen(): JSX.Element {
+  const prevRef = useRef<HTMLButtonElement>(null);
+  const nextRef = useRef<HTMLButtonElement>(null);
+
   return (
     <BaseContainer className="px-0">
       <div className="flex md:h-[223px] lg:h-[536px]">
@@ -111,41 +123,131 @@ export default function BlogScreen(): JSX.Element {
         </div>
       </div>
 
-      <div className="flex size-full justify-between font-bold">
-        <div className="flex justify-around gap-10">
-          <div className="flex items-center justify-center gap-1 text-primary">
-            <RxHamburgerMenu size={20} />
-            <span>Todos</span>
+      <div className="hidden lg:flex lg:flex-col">
+        <div className="flex size-full justify-between font-bold">
+          <div className="flex justify-around gap-10">
+            <div className="flex items-center justify-center gap-1 text-primary">
+              <RxHamburgerMenu size={20} />
+              <span>Todos</span>
+            </div>
+
+            <div className="flex cursor-pointer items-center justify-center gap-1 hover:text-darkGray">
+              <FaRunning size={20} />
+              <span>Prática de Esportes</span>
+            </div>
+
+            <div className="flex cursor-pointer items-center justify-center gap-1 hover:text-darkGray">
+              <GiFlowerStar size={20} />
+              <span>Saúde e Bem-estar</span>
+            </div>
+
+            <div className="flex cursor-pointer items-center justify-center gap-1 hover:text-darkGray">
+              <MdArrowOutward size={20} />
+              <span>Inovação & Oncologia</span>
+            </div>
           </div>
 
           <div className="flex cursor-pointer items-center justify-center gap-1 hover:text-darkGray">
-            <FaRunning size={20} />
-            <span>Prática de Esportes</span>
-          </div>
-
-          <div className="flex cursor-pointer items-center justify-center gap-1 hover:text-darkGray">
-            <GiFlowerStar size={20} />
-            <span>Saúde e Bem-estar</span>
-          </div>
-
-          <div className="flex cursor-pointer items-center justify-center gap-1 hover:text-darkGray">
-            <MdArrowOutward size={20} />
-            <span>Inovação & Oncologia</span>
+            <span>Mais Relevantes</span>
+            <IoIosArrowDown size={20} className="text-primary" />
           </div>
         </div>
 
-        <div className="flex cursor-pointer items-center justify-center gap-1 hover:text-darkGray">
-          <span>Mais Relevantes</span>
-          <IoIosArrowDown size={20} className="text-primary" />
+        <div className="mt-10">
+          {options.map((option) => (
+            <div className="mt-10" key={option.id}>
+              <NewsInformationCard options={option} />
+            </div>
+          ))}
         </div>
       </div>
 
-      <div className="mt-10">
-        {options.map((option) => (
+      <div className="flex flex-col lg:hidden">
+        <div className="flex justify-center">
+          <BaseSelect
+            color="primary"
+            variant="bordered"
+            className="max-w-[656px]"
+            radius="md"
+            options={[
+              { key: 1, value: "1", label: "Mais relevantes" },
+              { key: 2, value: "2", label: "Todos os temas" },
+            ]}
+          />
+        </div>
+        <div className="relative mx-10 w-full">
+          <Swiper
+            modules={[Navigation]}
+            slidesPerView={1}
+            spaceBetween={300}
+            loop
+            navigation={{
+              nextEl: nextRef.current,
+              prevEl: prevRef.current,
+            }}
+            onInit={(swiper) => {
+              if (swiper.params.navigation) {
+                const navigationParams = swiper.params
+                  .navigation as NavigationOptions;
+                navigationParams.prevEl = prevRef.current;
+                navigationParams.nextEl = nextRef.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+              },
+            }}
+          >
+            {options.map((option) => (
+              <SwiperSlide className="mt-10" key={option.id}>
+                <NewsInformationCard options={option} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+
+          <div className="mt-4 flex justify-center space-x-4">
+            <button
+              ref={prevRef}
+              type="button"
+              className="group flex size-10 items-center justify-center rounded-full bg-white/30 hover:bg-white/50 focus:outline-none dark:bg-gray-800/30 dark:hover:bg-gray-800/60"
+              aria-label="Previous"
+            >
+              <IoIosArrowBack
+                className="text-primary-foreground dark:text-gray-800"
+                size={35}
+              />
+            </button>
+
+            <button
+              ref={nextRef}
+              type="button"
+              className="group flex size-10 items-center justify-center rounded-full bg-white/30 hover:bg-white/50 focus:outline-none dark:bg-gray-800/30 dark:hover:bg-gray-800/60"
+              aria-label="Next"
+            >
+              <IoIosArrowForward
+                className="text-primary-foreground dark:text-gray-800"
+                size={35}
+              />
+            </button>
+          </div>
+        </div>
+        {/* {options.map((option) => (
           <div className="mt-10" key={option.id}>
             <NewsInformationCard options={option} />
           </div>
-        ))}
+        ))} */}
       </div>
     </BaseContainer>
   );
