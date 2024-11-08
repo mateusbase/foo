7d1b98b463c6949fa05240ac9f53ea187eb0084b
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2024-11-08 15:31:04 -0300

    fix: add swiper to information slider

--- src/components/InformationSlider/index.tsx
+++ src/components/InformationSlider/index.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
-import {
-  MdKeyboardArrowLeft,
-  MdOutlineKeyboardArrowRight,
-} from "react-icons/md";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { useSwiperNavigation } from "@/hooks/useSwiperNavigation";
 import { InformationSliderProps } from "./types";
 import { LogoIcon } from "../Icons";
+import SliderArrows from "../SliderArrows";
 
 export default function InformationSlider({
   title,
@@ -12,34 +12,55 @@ export default function InformationSlider({
   image,
   backgroundType = "gradient",
 }: InformationSliderProps): JSX.Element {
+  const { onBeforeInit, nextRef, prevRef, swiperRef } = useSwiperNavigation();
+
   return (
-    <div className="flex h-[536px] w-full justify-between text-white">
-      <div
-        className={`flex h-full w-1/2 flex-col justify-between gap-2 rounded-bl-[100px] px-20 py-10 ${backgroundType === "gradient"
-          ? "bg-gradient-to-r from-primary-foreground to-secondary-foreground"
-          : "bg-darkGray"
-          }`}
+    <div className="relative flex h-[536px] w-full justify-between text-white">
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={10}
+        loop
+        onBeforeInit={(swiper) => onBeforeInit(swiper)}
       >
-        <div className="mt-4">
-          <LogoIcon size={1} />
-        </div>
+        <SwiperSlide className="flex h-full">
+          <div
+            className={`flex h-full w-1/2 flex-col justify-between gap-2 rounded-bl-[100px] px-20 py-10 ${
+              backgroundType === "gradient"
+                ? "bg-gradient-to-r from-primary-foreground to-secondary-foreground"
+                : "bg-darkGray"
+            }`}
+          >
+            <div className="mb-2 mt-4">
+              <LogoIcon size={1} />
+            </div>
 
-        <div>
-          <p className="text-5xl">{title}</p>
-        </div>
+            <div>
+              <p className="mb-9 text-5xl">{title}</p>
+            </div>
 
-        <div>
-          <p className="text-2xl">{description}</p>
-        </div>
+            <div>
+              <p className="mb-32 text-2xl">{description}</p>
+            </div>
+          </div>
 
-        <div className="flex gap-2">
-          <MdKeyboardArrowLeft size="30px" />
-          <MdOutlineKeyboardArrowRight size="30px" />
-        </div>
-      </div>
+          <div className="h-full w-1/2">
+            <img
+              src={image}
+              alt="Imagem 1"
+              className="size-full object-cover"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
 
-      <div className="h-full w-1/2">
-        <img src={image} alt="Imagem 1" className="size-full" />
+      <div className="absolute bottom-5 left-[72px] z-10 flex">
+        <SliderArrows
+          swiperRef={swiperRef}
+          prevRef={prevRef}
+          nextRef={nextRef}
+          size={0.5}
+          color="text-white"
+        />
       </div>
     </div>
   );
