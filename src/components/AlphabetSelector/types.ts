c30100e683e7ce7dd9711608494295b3ed764aff
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2024-11-08 09:33:07 -0300

    Merge dev into 'feat/vaccines-screen'

--- src/components/AlphabetSelector/types.ts
+++ src/components/AlphabetSelector/types.ts
@@ -2,4 +2,6 @@ export interface AlphabetSelectorProps {
   selectedLetter: string;
   onLetterSelect: (letter: string) => void;
   searchPlaceholder?: string;
+  handleSearchChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  valueSearch?: string;
 }
