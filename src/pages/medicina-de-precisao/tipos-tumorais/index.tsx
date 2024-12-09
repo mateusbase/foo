0967590a3169df1750dbfbd4ee30bee1ddcdda33
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2024-12-09 11:26:55 -0300

    feat: add tumoral types screen

---
+++ src/pages/medicina-de-precisao/tipos-tumorais/index.tsx
@@ -0,0 +1,12 @@
+import Page from "@/components/Page";
+import TumorTypeScreen from "@/templates/TumorTypeScreen";
+
+const TumorTypes = (): JSX.Element => {
+  return (
+    <Page title="Tipos tumorais">
+      <TumorTypeScreen />
+    </Page>
+  );
+};
+
+export default TumorTypes;
