bfa934460411cd7d75c4c77937f1fe27e0f4442d
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2024-12-09 16:33:53 -0300

    feat: pathologic exams screen

---
+++ src/pages/medicina-de-precisao/patologia/index.tsx
@@ -0,0 +1,12 @@
+import Page from "@/components/Page";
+import PathologicExamsScreen from "@/templates/PathologicExamsScreen";
+
+const PatologicExams = (): JSX.Element => {
+  return (
+    <Page title="Patologia">
+      <PathologicExamsScreen />
+    </Page>
+  );
+};
+
+export default PatologicExams;
