c91a4798330df386641334d4d068e6992d155f87
Luiz Fernando <grexd@hotmail.com>
2024-12-16 15:09:23 -0300

    fix: :bug: adjusting talk to us screen in small cell phones

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -8,18 +8,24 @@ export default function InfoCard({
 }: MedicalServiceCardProps): JSX.Element {
   return (
     <div
-      className={`flex w-auto flex-col overflow-hidden ${roundedSide === "left" ? "rounded-bl-[100px]" : "rounded-br-[100px]"} bg-white`}
+      className={`flex w-auto flex-col overflow-hidden ${
+        roundedSide === "left" ? "rounded-bl-[100px]" : "rounded-br-[100px]"
+      } bg-white`}
     >
       <div
         className="h-[376px] bg-cover bg-no-repeat"
         style={{
-          backgroundImage:
-            backgroundImageUrl ??
-            "url('https://i.postimg.cc/3xkfhr8J/Captura-de-tela-2024-09-27-134021.png')",
+          backgroundImage: `url('${
+            backgroundImageUrl
+              ? `/assets/images/${backgroundImageUrl}`
+              : "https://i.postimg.cc/3xkfhr8J/Captura-de-tela-2024-09-27-134021.png"
+          }')`,
         }}
       />
       <div
-        className={`flex min-h-[292px] w-full flex-col justify-between gap-12 ${roundedSide === "left" ? "rounded-bl-[100px]" : "rounded-br-[100px]"} bg-gradient-to-br from-primary-foreground to-secondary-foreground px-14 py-8`}
+        className={`flex min-h-[292px] w-full flex-col justify-between gap-12 ${
+          roundedSide === "left" ? "rounded-bl-[100px]" : "rounded-br-[100px]"
+        } bg-gradient-to-br from-primary-foreground to-secondary-foreground px-14 py-8`}
       >
         {children}
       </div>
