e0fede6fc3f9c130d8a8744b7b21121d7a78da04
Luiz Fernando <grexd@hotmail.com>
2024-12-17 10:58:50 -0300

    fix: :bug: adjusting new layout in medic home screen

--- src/components/UpcomingEvents/index.tsx
+++ src/components/UpcomingEvents/index.tsx
@@ -1,98 +1,89 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-import { useRef } from "react";
 import { NavigationOptions } from "swiper/types";
+import { useSwiperNavigation } from "@/hooks/useSwiperNavigation";
 import EventCard from "../EventsCard";
 import { events } from "./eventMocks";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import PreviousArrowIcon from "../Icons/PreviousArrowIcon";
-import NextArrowIcon from "../Icons/NextArrowIcon";
+import SliderArrows from "../SliderArrows";
 
 export default function UpcomingEventsSection(): JSX.Element {
-  const prevRef = useRef<HTMLButtonElement>(null);
-  const nextRef = useRef<HTMLButtonElement>(null);
+  // const prevRef = useRef<HTMLButtonElement>(null);
+  // const nextRef = useRef<HTMLButtonElement>(null);
+  const { nextRef, prevRef, swiperRef } = useSwiperNavigation();
 
   return (
-    <div className="hidden items-start justify-center gap-4 px-4 py-16 lg:flex">
-      <div className="max-w-screen-lg">
-        <div className="w-full text-left">
-          <h2 className="text-5xl font-light text-primary">
-            Olá dr(a). Conheça a agenda dos próximos eventos Oncoclínicas
-          </h2>
-          <p className="mt-8 text-base text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-            hendrerit magna sit amet ultricies porttitor. Quisque fringilla ut
-            orci a venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Praesent hendrerit magna sit amet ultricies porttitor. Quisque
-            fringilla ut orci a venenatis.
-          </p>
+    <>
+      <div className="hidden items-start justify-center gap-4 px-4 py-16 lg:flex">
+        <div className="max-w-screen-lg">
+          <div className="w-full text-left">
+            <h2 className="text-5xl font-light text-primary">
+              Olá dr(a). Conheça a agenda dos próximos eventos Oncoclínicas
+            </h2>
+            <p className="mt-8 text-base text-gray-600">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              hendrerit magna sit amet ultricies porttitor. Quisque fringilla ut
+              orci a venenatis. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Praesent hendrerit magna sit amet ultricies
+              porttitor. Quisque fringilla ut orci a venenatis.
+            </p>
+          </div>
+        </div>
+
+        <div className="relative w-9/12 max-w-[1052px] md:max-w-[700px] lg:max-w-[1052px]">
+          <Swiper
+            modules={[Navigation]}
+            spaceBetween={0}
+            slidesPerView={1}
+            loop
+            navigation={{
+              nextEl: nextRef.current,
+              prevEl: prevRef.current,
+            }}
+            onInit={(swiper) => {
+              if (swiper.params.navigation) {
+                const navigationParams = swiper.params
+                  .navigation as NavigationOptions;
+                navigationParams.prevEl = prevRef.current;
+                navigationParams.nextEl = nextRef.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+              },
+            }}
+          >
+            {events.map((event) => (
+              <SwiperSlide key={event.title}>
+                <EventCard event={event} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
-
-      <div className="relative w-9/12 max-w-[1052px] md:max-w-[700px] lg:max-w-[1052px]">
-        <Swiper
-          modules={[Navigation]}
-          spaceBetween={0}
-          slidesPerView={1}
-          loop
-          navigation={{
-            nextEl: nextRef.current,
-            prevEl: prevRef.current,
-          }}
-          onInit={(swiper) => {
-            if (swiper.params.navigation) {
-              const navigationParams = swiper.params
-                .navigation as NavigationOptions;
-              navigationParams.prevEl = prevRef.current;
-              navigationParams.nextEl = nextRef.current;
-              swiper.navigation.init();
-              swiper.navigation.update();
-            }
-          }}
-          breakpoints={{
-            640: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 2,
-              spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 2,
-              spaceBetween: 30,
-            },
-          }}
-        >
-          {events.map((event) => (
-            <SwiperSlide key={event.title}>
-              <EventCard event={event} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
-        <div className="mt-4 flex justify-center space-x-4 lg:justify-end">
-          <button
-            ref={prevRef}
-            type="button"
-            className="group flex size-10 items-center justify-center rounded-full bg-white/30 hover:bg-white/50 focus:outline-none dark:bg-gray-800/30 dark:hover:bg-gray-800/60"
-            aria-label="Previous"
-          >
-            <PreviousArrowIcon className="text-primary dark:text-gray-800" />
-          </button>
-
-          <button
-            ref={nextRef}
-            type="button"
-            className="group flex size-10 items-center justify-center rounded-full bg-white/30 hover:bg-white/50 focus:outline-none dark:bg-gray-800/30 dark:hover:bg-gray-800/60"
-            aria-label="Next"
-          >
-            <NextArrowIcon className="text-primary dark:text-gray-800" />
-          </button>
-        </div>
+      <div className="flex justify-center">
+        <SliderArrows
+          swiperRef={swiperRef}
+          prevRef={prevRef}
+          nextRef={nextRef}
+          size={1}
+          color="text-primary"
+        />
       </div>
-    </div>
+    </>
   );
 }
