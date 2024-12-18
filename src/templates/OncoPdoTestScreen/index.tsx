f9df98fe76723295006800b666da9fb5d2bccfb0
Luiz Fernando <grexd@hotmail.com>
2024-12-18 11:27:13 -0300

    fix: :bug: fixing breadcrumb map and adding trade html tag

--- src/templates/OncoPdoTestScreen/index.tsx
+++ src/templates/OncoPdoTestScreen/index.tsx
@@ -7,7 +7,7 @@ export default function OncoPdoTestScreen(): JSX.Element {
   const testContent = `
     Criado para auxiliar na tomada de decisão dos tratamentos oncológicos, este teste permite analisar a resposta do tumor a diferentes combinações de quimioterápicos e terapias-alvo moleculares, auxiliando na determinação de um tratamento 100% personalizado, muito mais eficaz e assertivo.
 
-O Teste Onco-PDO™️ é realizado a partir de material tumoral, extraído por biópsia ou punção. As células coletadas passam por cultivo e tratamento, gerando aglomerados geneticamente idênticos ao tumor, chamados de PDOs (Patient-Derived Organoids).
+O Teste Onco-PDO™ é realizado a partir de material tumoral, extraído por biópsia ou punção. As células coletadas passam por cultivo e tratamento, gerando aglomerados geneticamente idênticos ao tumor, chamados de PDOs (Patient-Derived Organoids).
 
 A partir disso, são criados modelos in-vitro em 3D aos quais são aplicadas as possibilidades de tratamento escolhidas pelo médico. A resposta tumoral é quantificada e analisada para cada combinação de medicamentos e terapias, possibilitando identificar qual será mais assertiva para aquele paciente, individualmente.`;
 
@@ -15,12 +15,13 @@ A partir disso, são criados modelos in-vitro em 3D aos quais são aplicadas as 
 
   return (
     <PageLayout
-      title="Teste ONCO-PDO™️"
+      title="Teste ONCO-PDO&trade;"
       subtitle="Teste in-vitro que, a partir de modelos 3D obtidos do próprio tumor do paciente, avalia e quantifica a resposta das células tumorais a diferentes combinações de agentes quimioterápicos e terapias-alvo moleculares."
     >
       <div className="mt-10">
         <h1 className="text-4xl text-primary md:text-[40px]">
-          Onco-PDO™️ é o que há de mais moderno em Oncologia de Precisão
+          Onco-PDO<sup>&trade;</sup> é o que há de mais moderno em Oncologia de
+          Precisão
         </h1>
 
         <div className="my-10 w-full" />
@@ -39,7 +40,7 @@ A partir disso, são criados modelos in-vitro em 3D aos quais são aplicadas as 
 
       <div className="mt-20">
         <h1 className="text-4xl text-primary md:text-[40px]">
-          Os diferenciais do Teste Onco-PDO™️
+          Os diferenciais do Teste Onco-PDO<sup>&trade;</sup>
         </h1>
 
         <p className="mt-5 text-xl text-darkGray">
@@ -53,7 +54,8 @@ A partir disso, são criados modelos in-vitro em 3D aos quais são aplicadas as 
             aplicá-las ao paciente.
           </p>
           <p className="mt-5 text-xl text-darkGray">
-            Além disso, o Teste Onco-PDO™️ também se destacada por:
+            Além disso, o Teste Onco-PDO<sup>&trade;</sup> também se destacada
+            por:
           </p>
           <ul className="ml-4 list-disc text-xl text-darkGray">
             <li>
@@ -78,7 +80,7 @@ A partir disso, são criados modelos in-vitro em 3D aos quais são aplicadas as 
 
       <div className="mt-10">
         <h1 className="text-4xl text-primary md:text-[40px]">
-          Etapas do Teste Onco-PDO™️
+          Etapas do Teste Onco-PDO<sup>&trade;</sup>
         </h1>
         <div className="my-16 grid gap-10 md:grid-cols-2 lg:grid-cols-4">
           {stepTestData.map((step) => (
@@ -93,13 +95,14 @@ A partir disso, são criados modelos in-vitro em 3D aos quais são aplicadas as 
 
       <div className="my-10 w-5/6">
         <h1 className="text-4xl text-primary md:text-[40px]">
-          Em quais tumores pode ser aplicado o Teste Onco-PDO™️
+          Em quais tumores pode ser aplicado o Teste Onco-PDO<sup>&trade;</sup>
         </h1>
         <p className="mt-5 text-xl text-darkGray">
-          O Teste Onco-PDO™️ é indicado, principalmente, para pacientes em
-          estágio de câncer avançado, naqueles em que se observou o retorno do
-          crescimento do tumor após a terapia padrão, isto é, aqueles em que a
-          primeira linha de tratamento não mostrou uma resposta satisfatória.
+          O Teste Onco-PDO<sup>&trade;</sup> é indicado, principalmente, para
+          pacientes em estágio de câncer avançado, naqueles em que se observou o
+          retorno do crescimento do tumor após a terapia padrão, isto é, aqueles
+          em que a primeira linha de tratamento não mostrou uma resposta
+          satisfatória.
         </p>
         <p className="mt-5 text-xl text-darkGray">
           O teste está disponível para os seguintes tumores:
