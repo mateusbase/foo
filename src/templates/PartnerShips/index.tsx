92ff489bf0c5f2e690f75c646a26dd0fdb7e6055
Luiz Fernando <grexd@hotmail.com>
2024-12-23 16:46:43 -0300

    feat: :sparkles: creating mobile and tablet layout in partner screen

--- src/templates/PartnerShips/index.tsx
+++ src/templates/PartnerShips/index.tsx
@@ -3,16 +3,16 @@ import PageLayout from "@/components/PageLayout";
 export default function PartnerShipsScreen(): JSX.Element {
   return (
     <PageLayout title="Parcerias">
-      <div className="mt-10 max-h-[499px] w-full lg:flex">
-        <div className="max-w-[932px]">
+      <div className="full-bleed w-full lg:mt-10 lg:flex">
+        <div className="w-full lg:max-w-[932px]">
           <img
             src="/assets/images/partner-ships-banner.png"
             alt="Banner de parcerias"
-            className="lg:rounded-bl-[100px]"
+            className="size-full lg:rounded-bl-[100px]"
           />
         </div>
         <div className="w-full bg-gradient-to-b from-primary-foreground via-primary-foreground via-50% to-secondary-foreground lg:max-w-[604px]">
-          <h1 className="p-20 text-4xl text-white">
+          <h1 className="p-20 text-center text-3xl text-white md:text-4xl lg:text-left">
             As parcerias da Oncoclínicas&Co com empresas pelo Brasil tem como
             principal objetivo ampliar a oferta de serviços em toda a linha de
             cuidado tendo o paciente sempre como centro de tudo.
@@ -20,8 +20,8 @@ export default function PartnerShipsScreen(): JSX.Element {
         </div>
       </div>
 
-      <div className="my-28 w-full">
-        <p className="text-lg text-darkGray">
+      <div className="my-10 w-full lg:my-28">
+        <p className="text-sm text-darkGray md:text-xl">
           As parcerias da Oncoclínicas&Co com empresas pelo Brasil tem como
           principal objetivo ampliar a oferta de serviços em toda a linha de
           cuidado, desde o diagnóstico. O foco é não apenas expandir
@@ -29,12 +29,12 @@ export default function PartnerShipsScreen(): JSX.Element {
           multidisciplinaridades e as novas tecnologias oferecidas.
         </p>
 
-        <p className="mt-7 text-lg text-darkGray">
+        <p className="mt-7 text-sm text-darkGray md:text-xl">
           Nesse sentido, o grupo está alinhado com empresas conectadas com os
           seus valores, tendo o paciente sempre como centro de tudo e a
           excelência no atendimento e nos serviços oferecidos como prioridades.
         </p>
-        <p className="mt-7 text-lg text-darkGray">
+        <p className="mt-7 text-sm text-darkGray md:text-xl">
           Nossas parcerias são feitas para chegar a regiões e áreas onde ainda
           não estamos presentes, mas também para fortalecer o compartilhamento
           de experiências e inovações. Assim, contribuímos para o fortalecimento
