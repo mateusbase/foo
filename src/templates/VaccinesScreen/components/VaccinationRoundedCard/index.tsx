fb3888587a01a49f037c6ee2110fe7512bb3cced
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-01-08 09:58:50 -0300

    styles: adjust small screens layout

--- src/templates/VaccinesScreen/components/VaccinationRoundedCard/index.tsx
+++ src/templates/VaccinesScreen/components/VaccinationRoundedCard/index.tsx
@@ -9,17 +9,17 @@ const VaccinationRoundedCard = ({
 }: VaccinationRoundedCardProps): JSX.Element => {
   return (
     <InfoCard roundedSide="left" customPadding={4}>
-      <div className="flex min-h-[380px] flex-col justify-between p-4 lg:min-h-[530px] lg:p-6">
+      <div className="flex min-h-[300px] flex-col justify-between p-6 px-8 md:min-h-[380px] lg:min-h-[530px] lg:p-6">
         <h3 className="font-lato-thin text-3xl text-white lg:text-5xl">
           {title}
         </h3>
         <div className="mt-10 grid flex-1 grid-cols-1 gap-4 text-lg md:mt-14">
           {description ? (
-            <span className="font-lato-bold text-xl text-white md:text-lg lg:text-2xl">
+            <span className="mb-4 font-lato-bold text-base text-white md:text-lg lg:text-2xl">
               {description}
             </span>
           ) : (
-            <ul className="list-inside list-disc text-xl text-white md:text-lg lg:text-2xl">
+            <ul className="mb-4 list-inside list-disc text-base text-white md:text-lg lg:text-2xl">
               {listItems?.map((item) => (
                 <li className="font-lato-bold" key={item}>
                   {item}
--- src/templates/VaccinesScreen/components/VaccineLocations/index.tsx
+++ src/templates/VaccinesScreen/components/VaccineLocations/index.tsx
@@ -28,8 +28,11 @@ const VaccineLocations = (): JSX.Element => {
   return (
     <div className="mb-8 mt-2 flex w-full flex-col md:mt-12 lg:mt-28">
       <div className="flex w-full flex-col items-center md:flex-row md:gap-3">
-        <h1 className="mb-4 text-wrap text-center font-lato-bold text-2xl leading-relaxed text-primary md:mb-0 md:mr-4 md:w-[45%] md:text-left md:text-3xl lg:w-full lg:text-center lg:text-6xl">
-          Consulte as vacinas oferecidas na sua cidade
+        <h1 className="mb-4 w-full font-lato-bold text-3xl leading-relaxed text-primary md:mb-0 md:w-[45%] lg:w-full lg:text-6xl">
+          <span className="block text-left">
+            Consulte as vacinas oferecidas{" "}
+          </span>
+          <span className="block text-center">na sua cidade</span>
         </h1>
 
         <BaseSelect
