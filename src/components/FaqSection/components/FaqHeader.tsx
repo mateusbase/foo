a0c4b1b3df47ecf0d688e8086426b386499746b5
Luiz Fernando <grexd@hotmail.com>
2025-02-07 17:32:05 -0300

    fix: :bug: fixing leading on faq header on home screen

--- src/components/FaqSection/components/FaqHeader.tsx
+++ src/components/FaqSection/components/FaqHeader.tsx
@@ -2,7 +2,7 @@ import { FaqHeaderProps } from "./types";
 
 export const FaqHeader = ({ titleHeader }: FaqHeaderProps): JSX.Element => (
   <div className="mx-auto mb-8 w-full max-w-6xl text-center">
-    <h1 className="text-2xl font-light text-primary md:text-[40px]">
+    <h1 className="text-2xl font-light text-primary md:text-[40px] md:leading-10">
       {titleHeader}
     </h1>
   </div>
