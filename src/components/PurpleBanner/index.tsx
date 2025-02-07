0689411aad2f587eaae28b3fd9ae0c6fbc51d9d2
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-02-07 09:57:47 -0300

    feat: add precision medicine app screen

--- src/components/PurpleBanner/index.tsx
+++ src/components/PurpleBanner/index.tsx
@@ -4,18 +4,33 @@ const PurpleBanner = ({
   title,
   subtitle,
   description,
+  video,
 }: PurpleBannerProps): JSX.Element => {
   return (
-    <div className="relative left-1/2 flex size-full min-h-[302px] w-screen -translate-x-1/2 flex-col justify-center gap-4 bg-gradient-to-t from-purpleDark to-purpleLight px-10 py-4 md:-mt-5 lg:w-1/2 lg:rounded-bl-[100px] lg:p-20">
-      <p className="font-lato-regular text-2xl text-white md:text-4xl">
-        {title}
-      </p>
-      <p className="mt-6 font-lato-light text-3xl text-white md:text-4xl lg:text-5xl">
-        {subtitle}
-      </p>
-      <p className="mt-4 font-lato-bold text-lg text-white md:text-xl lg:text-2xl">
-        {description}
-      </p>
+    <div className="flex w-full flex-col md:mt-4 lg:flex-row">
+      <div className="relative left-1/2 flex size-full min-h-[302px] w-screen -translate-x-1/2 flex-col justify-center gap-4 bg-gradient-to-t from-purpleDark to-purpleLight px-10 py-4 md:-mt-5 lg:w-1/2 lg:rounded-bl-[100px] lg:p-20">
+        <p className="font-lato-regular text-2xl text-white md:text-4xl">
+          {title}
+        </p>
+        <p className="mt-6 font-lato-light text-3xl text-white md:text-4xl lg:text-5xl">
+          {subtitle}
+        </p>
+        <p className="mt-4 font-lato-bold text-lg text-white md:text-xl lg:text-2xl">
+          {description}
+        </p>
+      </div>
+
+      <div className="relative left-1/2 w-screen -translate-x-1/2 lg:w-auto lg:flex-1 lg:translate-x-0">
+        <video className="size-full object-cover" controls>
+          <source src={video} type="video/mp4" />
+          <track
+            src="captions_en.vtt"
+            kind="captions"
+            srcLang="en"
+            label="English"
+          />
+        </video>
+      </div>
     </div>
   );
 };
