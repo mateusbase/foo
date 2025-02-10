b527ab312e4a63dcb493c0ef9e4d258b46d7bb5e
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-02-10 10:45:28 -0300

    fix: list alignment

--- src/components/GradientBanner/index.tsx
+++ src/components/GradientBanner/index.tsx
@@ -8,7 +8,7 @@ export default function GradientBanner({
   backgroundType = "gradient",
 }: GradientBannerProps): JSX.Element {
   return (
-    <div className="full-bleed-md flex w-full justify-between text-white md:my-10 lg:h-[536px]">
+    <div className="full-bleed-md flex w-full justify-between text-white md:my-10 lg:min-h-[536px]">
       {sideImage === "left" ? (
         <div className="flex w-full flex-row">
           <div
