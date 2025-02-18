6e0ff1d0f759dfdd86c615f82f5cfffee3b9bd45
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-02-18 13:52:29 -0300

    feat: add prevention banner

--- src/templates/LivingWithCancerScreen/index.tsx
+++ src/templates/LivingWithCancerScreen/index.tsx
@@ -1,6 +1,7 @@
 import PageLayout from "@/components/PageLayout";
 import livingWithCancerMocks from "./livingWithCancerMcok";
 import RoundedCard from "./components/RoundedCard";
+import PreventionSection from "./components/PreventionSection";
 
 export default function LivingWithCancerScreen(): JSX.Element {
   return (
@@ -8,7 +9,7 @@ export default function LivingWithCancerScreen(): JSX.Element {
       title="Convivendo com o câncer"
       subtitle="A Oncoclínicas oferece suporte abrangente a pacientes com câncer, abordando diversas estratégias para melhorar a qualidade de vida e reduzir efeitos adversos que podem ocorrer durante o tratamento contra o câncer. "
     >
-      <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
+      <div className="my-14 grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
         {livingWithCancerMocks.map((item) => (
           <RoundedCard
             key={item.id}
@@ -18,6 +19,8 @@ export default function LivingWithCancerScreen(): JSX.Element {
           />
         ))}
       </div>
+
+      <PreventionSection />
     </PageLayout>
   );
 }
