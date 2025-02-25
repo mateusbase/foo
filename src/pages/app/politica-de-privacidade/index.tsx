a7f7d752979ed843c7210a9efda1c0071c3d8ff7
Guilherme Neves <guilh.neves@hotmail.com>
2025-02-25 10:07:27 -0300

    feat: :sparkles: creating privacy-policy screen on web, tablet and mobile layout

---
+++ src/pages/app/politica-de-privacidade/index.tsx
@@ -0,0 +1,12 @@
+import PrivacyPolicyScreen from "@/templates/PrivacyPolicyScreen";
+import Page from "@/components/Page";
+
+const PrivacyPolicy = (): JSX.Element => {
+  return (
+    <Page title="Política de privacidade e tratamento de dados do Oncoclínicas Por Você (“política”)">
+      <PrivacyPolicyScreen />
+    </Page>
+  );
+};
+
+export default PrivacyPolicy;
