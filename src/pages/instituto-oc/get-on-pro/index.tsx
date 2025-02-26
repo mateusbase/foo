f33092cfa5b092af1b5f5e0d8b1843b16522b2f8
Guilherme Neves <guilh.neves@hotmail.com>
2025-02-26 11:30:02 -0300

    feat: :sparkles: creating get-on-pro screen on web, tablet and mobile layout

---
+++ src/pages/instituto-oc/get-on-pro/index.tsx
@@ -0,0 +1,10 @@
+import Page from "@/components/Page";
+import GetOnPro from "@/templates/GetOnProScreen";
+
+export default function GetonPro(): JSX.Element {
+  return (
+    <Page title="Instituto Oncoclínicas" description=" Get On Pro">
+      <GetOnPro />
+    </Page>
+  );
+}
