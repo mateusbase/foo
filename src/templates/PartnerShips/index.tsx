99df07899e6969e44485533711e321a36173185b
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-03-06 13:52:53 -0300

    styles: change padding inside banner for small and medium screens

--- src/templates/PartnerShips/index.tsx
+++ src/templates/PartnerShips/index.tsx
@@ -12,7 +12,7 @@ export default function PartnerShipsScreen(): JSX.Element {
           />
         </div>
         <div className="w-full bg-gradient-to-b from-primary via-primary via-70% to-secondary lg:max-w-[604px]">
-          <h1 className="p-20 text-center text-3xl text-white md:text-4xl lg:text-left">
+          <h1 className="px-10 py-14 text-center text-3xl text-white md:text-4xl lg:text-left">
             As parcerias da Oncoclínicas&Co com empresas pelo Brasil tem como
             principal objetivo ampliar a oferta de serviços em toda a linha de
             cuidado tendo o paciente sempre como centro de tudo.
