b8329f8299f23f9783162e8e6c13d0dcc8198521
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-03-06 13:35:01 -0300

    fix: adjust spacing and dimensions in HealthSpaceBanner and BenefitsCards components; add duplicate video entry in VideosSwiper

--- src/templates/OcWomanScreen/components/benefitsCards/index.tsx
+++ src/templates/OcWomanScreen/components/benefitsCards/index.tsx
@@ -3,7 +3,7 @@ import { WomanBenefitsMock } from "../../interfaces";
 const BenefitsCards = (props: WomanBenefitsMock): JSX.Element => {
   const { title, description } = props;
   return (
-    <div className="mx-auto my-12 h-[390px] w-[90%] rounded-xl border border-darkGray px-6 pb-20 pt-8 md:w-[98%]">
+    <div className="mx-auto my-12 h-[390px] w-[90%] rounded-xl border border-darkGray px-6 pb-20 pt-8 md:h-[330px] md:w-[98%] lg:h-[390px] xl:h-[300px]">
       <h2 className="mb-8 text-2xl text-primary">{title}</h2>
       <p className="text-base text-darkGray">{description}</p>
     </div>
