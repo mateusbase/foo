42b484b246de841640913c7942b44aac0aeb73da
Guilherme Neves <guilh.neves@hotmail.com>
2025-03-06 11:31:30 -0300

    style: :gem: align texts on cards and adjust margin and text according to device

--- src/templates/VeinviewerScreen/components/VeinviewerBanner/index.tsx
+++ src/templates/VeinviewerScreen/components/VeinviewerBanner/index.tsx
@@ -4,7 +4,7 @@ const VeinviewerBanner = (): JSX.Element => {
       <img
         src="/assets/images/veinviewer/banner-veinviewer-1.png"
         alt="Veinviewer"
-        className="hidden w-1/2 rounded-bl-[80px] lg:block"
+        className="hidden rounded-bl-[80px] lg:block"
         fetchPriority="high"
       />
       <img
@@ -14,8 +14,8 @@ const VeinviewerBanner = (): JSX.Element => {
         fetchPriority="high"
       />
 
-      <div className="flex h-[375px] flex-col bg-custom-gradient-dark p-9 text-center text-white lg:h-[405px]">
-        <p className="mx-auto mt-7 max-w-[20ch] font-lato-light text-3xl md:mt-7 md:max-w-[40ch] md:text-4xl lg:mr-24 lg:mt-12 lg:max-w-[20ch] lg:pl-6 lg:text-left">
+      <div className="flex h-[375px] flex-col bg-custom-gradient-dark p-9 text-center text-white lg:h-[499px] lg:w-full">
+        <p className="mx-auto mt-7 max-w-[30ch] font-lato-light text-3xl md:mt-20 md:text-4xl lg:mr-24 lg:mt-12 lg:max-w-[20ch] lg:pl-6 lg:text-left">
           Veinviewer é uma tecnologia de visualização venosa de última geração
           oferecida pela Oncoclínicas&Co.
         </p>
