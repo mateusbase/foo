a91d6db07cb2f8632bde1b64376917134bd1d547
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-03-06 11:51:57 -0300

    refactor: adjust alphabet selector and card sizes

--- src/templates/ServicesScreen/components/ServiceCard/index.tsx
+++ src/templates/ServicesScreen/components/ServiceCard/index.tsx
@@ -9,14 +9,14 @@ export default function ServiceCard({
   showButton = true,
 }: ServiceCardProps): JSX.Element {
   return (
-    <div className="flex min-h-[231px] w-full flex-col rounded-[20px] border border-lightGray bg-white px-3 py-5 lg:h-[272px] lg:justify-between lg:p-6">
-      <div className="flex flex-col items-start lg:flex-row">
+    <div className="flex min-h-[253px] w-full grow flex-col rounded-[20px] border border-lightGray bg-white px-3 py-5 md:min-h-[330px] lg:h-[272px] lg:justify-between lg:p-6">
+      <div className="flex flex-col items-start lg:flex-row lg:items-center">
         <div className="flex size-11 items-center justify-center rounded-full bg-primary-foreground lg:size-[63px]">
           {serviceIcon}
         </div>
 
         <div>
-          <h3 className="text-sm leading-4 text-primary sm:mt-0 sm:leading-[32px] md:font-normal lg:ml-4 lg:text-[22px]">
+          <h3 className="align-middle text-sm leading-4 text-primary sm:mt-0 sm:leading-[32px] md:font-normal lg:ml-4 lg:text-sm xl:text-[22px]">
             {serviceTitle}
           </h3>
         </div>
