9b94f67e7d78e01c96266074f021fab48267ca33
Guilherme Neves <guilh.neves@hotmail.com>
2025-03-19 10:14:04 -0300

    style: :gem: adjust heading width in PurpleFullBleedBanner and add margin in RightFullbleedBanner

--- src/templates/OcFranchiseScreen/components/PurpleFullBleedBanner/index.tsx
+++ src/templates/OcFranchiseScreen/components/PurpleFullBleedBanner/index.tsx
@@ -14,7 +14,7 @@ const PurpleFullbleedBanner = ({
   return (
     <div className="full-bleed mb-14 flex flex-col-reverse md:flex-row-reverse md:items-stretch lg:mt-12">
       <div className="flex h-[350px] flex-col gap-2 bg-gradient-to-b from-purpleDark to-purpleLight py-11 pl-8 pr-16 font-lato-regular text-white md:h-[400px] md:w-1/2 md:pl-8 md:pr-20 md:pt-14 lg:h-[479px] lg:lg:pb-12 lg:pl-16 lg:pr-11 lg:pt-[71px]">
-        <h1 className="mb-8 w-[70%] text-2xl lg:text-3xl xl:text-4xl">
+        <h1 className="mb-8 w-[90%] text-2xl lg:w-4/5 lg:text-3xl xl:text-4xl">
           {title}
         </h1>
         {description && (
