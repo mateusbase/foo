b6c645e8e5803978ebc9d697bbfbd4e75af1bf00
Guilherme Neves <guilh.neves@hotmail.com>
2025-03-20 14:37:15 -0300

    style: gem: adjust PalliativeCareCard icons, add video, fix the banner at the end of page

--- src/templates/OcPalliativeCareScreen/components/PalliativeCareCard/index.tsx
+++ src/templates/OcPalliativeCareScreen/components/PalliativeCareCard/index.tsx
@@ -1,17 +1,17 @@
-import { RiMentalHealthLine } from "react-icons/ri";
+import Image from "next/image";
 import { PalliativeCareCardProps } from "./types";
 
 export default function PalliativeCareCard({
   serviceName,
-  iconCard = <RiMentalHealthLine size={34} />,
+  iconCard,
 }: PalliativeCareCardProps): JSX.Element {
   return (
-    <div className="flex min-h-44 w-full items-center rounded-2xl border-1 border-lightGray py-2 pl-6 md:min-h-28">
+    <div className="flex min-h-44 w-full items-center rounded-2xl border-1 border-lightGray py-4 pl-6 pr-3 md:min-h-28">
       <div className="flex items-center gap-4">
         <div className="flex size-14 min-w-14 items-center justify-center rounded-full bg-primary-foreground text-white">
-          {iconCard}
+          <Image src={iconCard} alt="Ícone" width={60} height={60} />
         </div>
-        <div className="text-lg leading-tight text-darkGray 2xl:text-xl">
+        <div className="text-base leading-tight text-darkGray 2xl:text-lg">
           {serviceName}
         </div>
       </div>
