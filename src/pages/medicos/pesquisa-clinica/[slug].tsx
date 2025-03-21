a78e678a1067e4651be163c582efec3bae2db211
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-03-21 13:44:08 -0300

    fix: adjust slug to show only one navbar

--- src/pages/medicos/pesquisa-clinica/[slug].tsx
+++ src/pages/medicos/pesquisa-clinica/[slug].tsx
@@ -1,3 +1,13 @@
+import Page from "@/components/Page";
 import HowItWorksScreen from "@/templates/HowItWorksScreen";
 
-export default HowItWorksScreen;
+export default function HowItWorks(): JSX.Element {
+  return (
+    <Page
+      title="Oncoclínicas - Referência em tratamento oncológico | Pesquisa Clínica"
+      description="Pesquisa Clínica"
+    >
+      <HowItWorksScreen />
+    </Page>
+  );
+}
