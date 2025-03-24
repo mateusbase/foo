92d0f4971ca0cf7944d88f58314f405d91311b91
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-03-24 12:48:18 -0300

    style: refactor SpecialtyCard layout for improved spacing and alignment

--- src/templates/SpecialtysScreen/components/SpecialtyCard/index.tsx
+++ src/templates/SpecialtysScreen/components/SpecialtyCard/index.tsx
@@ -2,12 +2,12 @@ import { IoChevronForwardCircleOutline } from "react-icons/io5";
 
 export function SpecialtyCard({ name }: { name: string }): JSX.Element {
   return (
-    <div className="flex h-[105px] w-full cursor-pointer items-center gap-4 rounded-lg border border-primary px-10 py-2 text-primary xl:px-12 xl:py-3">
+    <div className="flex h-[105px] w-full cursor-pointer items-center rounded-lg border border-primary px-6 py-2 text-primary xl:px-8 xl:py-3">
       <div className="flex w-full items-center justify-between">
-        <h2 className="font-lato-bold text-xl lg:text-base xl:text-xl">
+        <h2 className="flex-1 text-left font-lato-bold text-xl lg:text-base xl:text-xl">
           {name}
         </h2>
-        <IoChevronForwardCircleOutline className="shrink-0 text-3xl text-primary" />
+        <IoChevronForwardCircleOutline className="text-3xl text-primary" />
       </div>
     </div>
   );
