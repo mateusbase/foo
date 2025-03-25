b52ec62cb104cb7f7ca6c468231fd7205215ce46
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-03-25 17:37:53 -0300

    remove: delete unused libs and archives

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import ErrorBoundary from "@/components/ErrorBoundary";
-import { useApollo } from "@/hooks/useApollo";
 // import VLibras from "vlibras-nextjs";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "@/styles/globals.css";
-import { ApolloProvider } from "@apollo/client";
 import { CookiesProvider } from "react-cookie";
 import { HeroUIProvider } from "@heroui/react";
 import { Lato } from "next/font/google";
@@ -27,27 +25,23 @@ export default function App({
   const getLayout = Component.getLayout ?? ((page) => page);
   const methods = useForm();
 
-  const apolloClient = useApollo(pageProps.initialApolloState);
-
   return (
     <HeroUIProvider>
       <CookiesProvider>
-        <ApolloProvider client={apolloClient}>
-          <LanguageProvider>
-            <SearchProvider>
-              <FormProvider {...methods}>
-                <ErrorBoundary>
-                  {getLayout(
-                    <main className={lato.className}>
-                      <Component {...pageProps} />
-                    </main>,
-                  )}
-                </ErrorBoundary>
-                {/* {process.env.NODE_ENV === "production" && <VLibras forceOnload />} */}
-              </FormProvider>
-            </SearchProvider>
-          </LanguageProvider>
-        </ApolloProvider>
+        <LanguageProvider>
+          <SearchProvider>
+            <FormProvider {...methods}>
+              <ErrorBoundary>
+                {getLayout(
+                  <main className={lato.className}>
+                    <Component {...pageProps} />
+                  </main>,
+                )}
+              </ErrorBoundary>
+              {/* {process.env.NODE_ENV === "production" && <VLibras forceOnload />} */}
+            </FormProvider>
+          </SearchProvider>
+        </LanguageProvider>
       </CookiesProvider>
     </HeroUIProvider>
   );
