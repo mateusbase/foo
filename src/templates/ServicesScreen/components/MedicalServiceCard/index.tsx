7cf8bbd7e1ab6854cffbba26058449d52ccf0e5f
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2025-03-27 10:03:25 -0300

    style: update MedicalServiceCard layout and improve styling for better responsiveness

--- src/templates/ServicesScreen/components/MedicalServiceCard/index.tsx
+++ src/templates/ServicesScreen/components/MedicalServiceCard/index.tsx
@@ -8,7 +8,7 @@ export default function MedicalServiceCard({
   serviceTitle,
   serviceDescription,
   actionButtonText,
-  serviceIcon = <Plus size={22} />,
+  serviceIcon = <Plus size={22} className="shrink-0" />,
   backgroundImageUrl,
   imageClassName,
   shouldShowButton = true,
@@ -17,26 +17,26 @@ export default function MedicalServiceCard({
   const hasSubtitle = !!subtitle;
 
   return (
-    <div className="flex h-fit w-auto flex-col overflow-hidden rounded-bl-[50px] border bg-white lg:rounded-bl-none lg:rounded-br-[100px]">
+    <div className="flex h-fit w-auto flex-col rounded-bl-[50px] border bg-white lg:rounded-bl-none lg:rounded-br-[100px]">
       <div
-        className={`${imageClassName} bg-contain bg-no-repeat`}
+        className={`${imageClassName} bg-cover bg-no-repeat`}
         style={{
           backgroundImage:
             backgroundImageUrl ??
             "url('https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/alt-5ae892611bf1a-5168-68b2575aab38f2c97ce8846381d07044@1x.jpg')",
         }}
       />
-      <div className="flex h-[289px] min-w-[322px] flex-col bg-custom-gradient-dark-145deg p-6 md:px-9 lg:h-[342px] lg:min-h-[515px] lg:gap-12 lg:rounded-br-[100px] lg:bg-custom-gradient-dark">
+      <div className="flex h-[289px] min-w-[322px] flex-col rounded-bl-[50px] bg-custom-gradient-dark-145deg p-6 md:px-9 lg:h-[342px] lg:min-h-[500px] lg:gap-6 lg:lg:rounded-bl-none lg:rounded-br-[100px] lg:bg-custom-gradient-dark">
         {hasSubtitle && (
-          <p className="mt-4 text-left text-lg leading-[30px] text-white md:text-[24px]">
+          <p className="mb-4 text-left text-lg leading-[30px] text-white md:text-[24px] lg:mt-7">
             {subtitle}
           </p>
         )}
 
         {hasActionButton && (
-          <div className="mb-8 w-3/4 md:w-11/12">
+          <div className="mb-4 w-[41%] lg:mt-7">
             <BaseButton
-              className="h-10 w-full rounded-2xl border-white text-white"
+              className="h-10 rounded-2xl border-2 border-white text-center text-[13px] text-white"
               variant="bordered"
               startContent={serviceIcon}
             >
@@ -50,7 +50,7 @@ export default function MedicalServiceCard({
             {serviceTitle}
           </h3>
 
-          <p className="mt-4 text-sm leading-5 text-white md:text-sm lg:text-xl lg:leading-[30px] 2xl:text-2xl">
+          <p className="mt-4 text-sm leading-5 text-white md:text-sm lg:mt-8 lg:text-xl lg:leading-[30px] 2xl:text-2xl">
             {serviceDescription}
           </p>
         </div>
